ac5c19766eb285b83e94c6481d24ae34bac05373
Anthony Ellis <amellis245@gmail.com>
2021-12-18 13:01:01 -0500

    created new object store for offline-data

--- public/offlineDB.js
+++ public/offlineDB.js
@@ -1,3 +1,12 @@
 // Created new DB
 let db;
 const request = indexedDB.open("budget", 1);
+
+// Create new Object Store
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    // ID for offline db data
+    keyPath: "id",
+    autoIncrement: true,
+  });
+};
