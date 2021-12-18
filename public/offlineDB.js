d4d81147c5ffc8cac9b73f6e9bb0c0342d2b6572
Anthony Ellis <amellis245@gmail.com>
2021-12-18 13:18:05 -0500

    added a post request on the condition of a successful retrival from the database where the transaction array > 0

--- public/offlineDB.js
+++ public/offlineDB.js
@@ -42,4 +42,26 @@ function checkDatabase() {
 
   // Get all offline transactions from Object Store
   const getAll = store.getAll();
+
+  // On successful retrival of offline data (offline-transaction array > 0)
+  // POST data in bulk
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // Clear object store after successful POST
+          const transaction = db.transaction("pending", "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
 }
