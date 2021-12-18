de3dfb1946775a9d21a0e442036c43832dfa7e45
Anthony Ellis <amellis245@gmail.com>
2021-12-18 11:44:02 -0500

    added event listener to fetch clone and store data in the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,3 +44,29 @@ self.addEventListener("activate", (evt) => {
 
   self.clients.claim();
 });
+
+// Fetches data
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/")) {
+    console.log("Service Worker Fetch data:", evt.request.url);
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+});
