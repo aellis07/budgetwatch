82836fa979cdfe23147a7e91a7c134c63897a7a3
Anthony Ellis <amellis245@gmail.com>
2021-12-18 00:38:18 -0500

    added static cache for public files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,23 @@
 console.log("Service worker ready to work");
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "BudgetWatch-FileCache";
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
