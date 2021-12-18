33dc2fc7350f5c0fdcb112d22c083b3c9d072584
Anthony Ellis <amellis245@gmail.com>
2021-12-18 11:32:29 -0500

    updated file-to-cache list

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,12 +5,16 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/styles.css",
   "/index.js",
+  "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
 const CACHE_NAME = "BudgetWatch-FileCache";
 
+// Intakes cache data
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
