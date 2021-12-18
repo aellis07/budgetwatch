ebb60c282b9b4e965da399537dc93a04c1423327
Anthony Ellis <amellis245@gmail.com>
2021-12-18 13:03:24 -0500

    added saveRecord function to save offline transactions

--- public/offlineDB.js
+++ public/offlineDB.js
@@ -10,3 +10,26 @@ request.onupgradeneeded = (event) => {
     autoIncrement: true,
   });
 };
+
+// Checks to see if app is online
+request.onsuccess = (event) => {
+  db = event.target.result;
+
+  // If online check db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = (err) => {
+  console.log(err.message);
+};
+
+// saveRecord(); is called in index.js
+// Takes offline transactions and saves it to the budget DB
+// Takes offline transactions and list them in a pending Object Store
+function saveRecord(record) {
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
